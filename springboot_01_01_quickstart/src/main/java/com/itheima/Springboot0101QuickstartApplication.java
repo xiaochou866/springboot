2f4876ddcddd1cf8bb6a1ebe9fb82f25a19d51f5
Joker <1574427203@qq.com>
2022-09-23 22:07:33 +0800

    20220923配置文件

--- springboot_01_01_quickstart/src/main/java/com/itheima/Springboot0101QuickstartApplication.java
+++ springboot_01_01_quickstart/src/main/java/com/itheima/Springboot0101QuickstartApplication.java
@@ -1,13 +1,17 @@
 package com.itheima;
 
+import com.itheima.controller.BookController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot0101QuickstartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+		BookController bean = ctx.getBean(BookController.class);
+		System.out.println("bean=====>"+bean);
 	}
 
 }
